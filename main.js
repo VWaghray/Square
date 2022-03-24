f7dd5525d381cfe63e878f64755dcff055b40ebb
VWaghray <87341415+VWaghray@users.noreply.github.com>
2022-03-24 17:11:28 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+video =  createCapture(VIDEO);
+  video.size(550, 500);
+  
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+}
+
+function draw(){
+  background('#969A97');
+}
